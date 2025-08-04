e24bc9c64e68406670e6fc7b38be4d55d22ddc6e
Manny <manny.blum@gmail.com>
2025-08-04 11:09:41 -0400

    tweaks for upcoming submodule

---
+++ src/index.tsx
@@ -0,0 +1,1 @@
+export { default as TriviaQuiz } from "./TriviaQuiz";
--- vite.config.ts
+++ vite.config.ts
@@ -2,17 +2,21 @@ import { defineConfig } from "vite";
 // import react from "@vitejs/plugin-react-swc";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
+  plugins: [react(), tailwindcss()],
   build: {
-    outDir: "build",
+    outDir: "dist",
     lib: {
-      entry: "src/main.tsx",
-      name: "TriviaQuizSubmodule",
-      fileName: "triviaquiz-submodule",
+      entry: path.resolve(__dirname, "src/index.tsx"),
+      name: "TriviaQuiz",
+      fileName: () => "triviaquiz.js",
       formats: ["es"],
     },
+    rollupOptions: {
+      external: ["react", "react-dom"],
+    },
   },
-  plugins: [react(), tailwindcss()],
 });
